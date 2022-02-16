fb5a93f581139487a1e294e14a1c45e1a00f7502
Priyanshu-2001 <priyanshuhupta123123@gmail.com>
2022-02-16 16:40:03 +0530

    Initial commit

---
+++ app/src/main/java/com/geek/runtracker/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.geek.runtracker.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geek.runtracker.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
